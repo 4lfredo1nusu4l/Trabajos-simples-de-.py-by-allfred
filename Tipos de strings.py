043566425b0b612f477ab09a5cbc73a848c76e37
4lfredo1nusu4l <153150878+4lfredo1nusu4l@users.noreply.github.com>
2024-04-26 21:26:54 -0600

    Add files via upload
    Este el el segundo trabajo de "python" que hago espero lo puedad usar :)

---
+++ Tipos de strings.py
@@ -0,0 +1,10 @@
+texto = "Hola Mundo"
+print (texto.upper())
+
+texto = "Hola Mundo"
+print (texto.find("M"))
+
+texto = "Hola Mundo"
+print (texto.replace("Mundo", "chanchito feliz" ))
+
+print ("Mundo" in texto  )
